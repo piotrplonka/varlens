a1818e8ddcd5a5e43017d88314599ea03a3e8ba3
piotrplonka <132056621+piotrplonka@users.noreply.github.com>
2024-12-12 18:27:46 +0100

    Update skrypt_fnpeaks.sh

--- skrypt_fnpeaks.sh
+++ skrypt_fnpeaks.sh
@@ -8,7 +8,7 @@ export OUTPUT_FILE
 
 find "$DATA_DIR" -name "*.dat" | parallel -j$(nproc) '
   filename=$(basename {})
-  ./fnpeaks_pro {} 0.01 250 0.001 | sed "s|{}|$filename|" | tee -a "$OUTPUT_FILE"
+  ./fnpeaks_pro {} 0.01 100 0.0001 | sed "s|{}|$filename|" | tee -a "$OUTPUT_FILE"
 
 echo "Przetwarzanie zakończone. Wyniki zapisano w $OUTPUT_FILE."
 
