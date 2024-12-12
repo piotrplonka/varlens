f747d348bc67f9a3a51d627748cc0117a1222318
piotrplonka <132056621+piotrplonka@users.noreply.github.com>
2024-12-12 19:23:00 +0100

    Add files via upload

--- skrypt_AOV.sh
+++ skrypt_AOV.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-
 OUTPUT_FILE="results_AOV.txt"
-
 > "$OUTPUT_FILE"
 
-export OUTPUT_FILE
 
-find . -maxdepth 1 -name "*.dat" | parallel -j$(nproc) '
-  filename=$(basename {})
-  ./AOV_pro {} | sed "s|{}|$filename|" | tee -a "$OUTPUT_FILE"
-'
+process_file() {
+  dat_file="$1"
+  ./AOV_pro "$dat_file"| sed "s|$dat_file|$(basename "$dat_file")|" >> "$OUTPUT_FILE"
+  echo "Przetwarzanie "$dat_file""
+}
+
+export OUTPUT_FILE
+export -f process_file
+
+find . -name "*.dat" | parallel -j$(nproc) process_file
+
 echo "Przetwarzanie zakończone. Wyniki zapisano w $OUTPUT_FILE."
 
